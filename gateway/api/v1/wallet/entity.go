c9ae1d14fcd037224a93d9fa4c147698df37c5e2
adonis developer <nvvan.0901.developer@gmail.com>
2025-09-10 09:19:41 +0700

    feat: implement create implicit account when register

--- gateway/api/v1/wallet/entity.go
+++ gateway/api/v1/wallet/entity.go
@@ -8,10 +8,10 @@ import (
 
 type Wallet struct {
 	gorm.Model
-	UserID            uint   `gorm:"uniqueIndex"` // one-to-one: mỗi user chỉ có 1 wallet
-	AccountID         string `gorm:"size:128"`    // Implicit account ID (hex of public key)
-	PublicKey         string `gorm:"size:128"`    // ed25519:base64
-	EncryptPrivateKey string `gorm:"size:128"`    // ed25519:base64; encrypt in production!
+	UserID            uint   `gorm:"uniqueIndex"`       // one-to-one: mỗi user chỉ có 1 wallet
+	AccountID         string `gorm:"size:128"`          // Implicit account ID (hex of public key)
+	PublicKey         string `gorm:"size:128"`          // ed25519:base64
+	EncryptPrivateKey string `gorm:"size:255" json:"-"` // ed25519:base64; encrypt in production!
 
 	User users.User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:UserID"`
 }
